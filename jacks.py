c5c7084f62f3173ee447c7990ff7b79116b0e7f9
Alfie Ford <alfie.ford@grantowngrammar.org.uk>
2023-03-21 08:57:44 +0000

    Update jacks.py

--- jacks.py
+++ jacks.py
@@ -1,16 +1,25 @@
-import jacks_source
+import jacks
+from colorama import Fore, Back, Style
 import timeit
-runtumestart = timeit.default_timer()
+runtimestart = timeit.default_timer()
 times = []
-for i in range(10):
-    try:
-        startTime = timeit.default_timer()
-        jacks_source.jacks()
-        endTime = timeit.default_timer()
-        times.append((endTime - startTime))
-    except IndexError:
-        pass
+for i in range(10000): #Change the number in the range to ajust how many simulations to run
+  try:  
+    #print("Game no" , Fore.RED + str(i))
+    #print(Style.RESET_ALL)
+    times.append((jacks.jacks()*2))
+  except IndexError:
+    pass
 runtimeend = timeit.default_timer()
-print("Average game time",((sum(times) / len(times))/60))
-print("Max game time",(max(times)/60))
-print("Sim time" ((runtimestart - runtimeend)/60))
+    
+average = ((sum(times) / len(times))/60)
+maxtime = (max(times)/60)
+print("Results:")
+print("Average game time:",round(average,2),"mins")
+print("Max game time:",round(maxtime,2),"mins")
+print("Simulation took",((runtimeend - runtimestart)),"seconds")
+  
+f = open("results.csv", "a")
+output = "\n" + str(round(average,2)) + "," + str(round(maxtime,2))
+f.write(output)
+f.close()
